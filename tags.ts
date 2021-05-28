6b375567db776393987979bf58c9a8329d839563
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-05-28 15:10:28 +0200

    Poprawiono błąd z rozdzielczością eksportowanych obrazów

--- tags.ts
+++ tags.ts
@@ -2,9 +2,9 @@ import { fromUint8Array } from "https://denopkg.com/chiefbiiko/base64/mod.ts";
 
 const tags: string[] = [];
 
-for await (const dirEntry of Deno.readDir("./input")) {
+for await (const dirEntry of Deno.readDir("./output")) {
     console.log(dirEntry.name);
-    const img = await Deno.readFile("./input/" + dirEntry.name);
+    const img = await Deno.readFile("./output/" + dirEntry.name);
     tags.push(`<img alt"${dirEntry.name.replace(".jpg", "")}" src="data:image/jpg;base64,${fromUint8Array(img)}">`);
 }
 Deno.writeTextFileSync("tags.html", tags.join("\n"));
